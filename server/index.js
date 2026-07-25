1f43f98bb03e8d338d4185fea8db4652b3bb0f54
EldorbekDev <eldorbekrajabboyev2007@gmail.com>
2026-07-25 08:40:14 +0500

    fix: add missing crypto import for admin login endpoint

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const crypto = require('crypto');
 const path = require('path');
 const multer = require('multer');
 const jwt = require('jsonwebtoken');
